2e9c9676295a5a3207aeaff6302ee66c8f68d1b3
Ihsan Maulana Hamdani <32558824+ihsanmaulanahamdani@users.noreply.github.com>
2024-05-22 08:11:14 +0700

    Create convertUnixToDate.js

---
+++ convertUnixToDate.js
@@ -0,0 +1,11 @@
+/*
+Gunakan npm dayjs (https://www.npmjs.com/package/dayjs) untuk menyelesaikan tugas ini.
+Convert Unix timestamp menjadi tanggal dengan format DD/MM/YYYY
+*/
+
+function convertUnixToDate(unixTimestamp) {
+  // Code here
+}
+
+convertUnixToDate(1710867600) // 20/03/2024
+convertUnixToDate(1714150800) // 27/04/2024
