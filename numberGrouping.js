e6b9bce9b8cc78064fda4b0895495bd567561d8b
gufranlazuardi <gufranlazuardi@gmail.com>
2024-05-24 20:15:19 +0700

    fix : number grouping

--- numberGrouping.js
+++ numberGrouping.js
@@ -11,18 +11,18 @@ function numberGrouping(numbers) {
   const dibagiTiga = [];
 
   numbers.forEach((num) => {
-    if (num % 2 === 0) {
-      genap.push(num);
-    }
-    if (num % 2 !== 0) {
-      ganjil.push(num);
-    }
     if (num % 3 === 0) {
       dibagiTiga.push(num);
+    } else if (num % 2 === 0) {
+      genap.push(num);
+    }
+
+    if (num % 2 !== 0) {
+      ganjil.push(num);
     }
   });
 
   return console.log([genap, ganjil, dibagiTiga]);
 }
 
-numberGrouping([1, 2, 4, 6, 8]); // Output: [[2, 4, 6, 8], [1], [6]]
+numberGrouping([1, 2, 4, 6, 8]); // Output: [[2, 4, 8], [1], [6]]
